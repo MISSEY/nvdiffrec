0329ec69ecf758b2918a2bf5feb47042b79a8183
Saurabh Mishra <sami02@dfki.de>
2023-12-01 12:59:46 +0100

    add run.sh to support hpc cluster at DFKI

---
+++ docker/run.sh
@@ -0,0 +1,4 @@
+pip install --global-option="--no-networks" git+https://github.com/NVlabs/tiny-cuda-nn#subdirectory=bindings/torch
+imageio_download_bin freeimage
+cd nvdiffrec
+python train.py --config configs/bob.json
